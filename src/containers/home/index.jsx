71fc85a55e66e0e581b1c5af226a8bc3d2b8045b
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-07-23 23:13:02 +0530

    add partciles in homepage

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import { particles } from '../../utils/particles';
 
 const Home = () => {
+
+  const particlesInit = useCallback(async main => {
+    await loadFull(main);
+  }, []);
+
   return (
-    <div></div>
+    <div>
+        {(
+          <Particles
+            id='tsparticles'
+            init={particlesInit}
+            options={particles}
+          />
+        )}
+    </div>
   )
 }
 
