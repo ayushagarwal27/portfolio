bf9646b1fe0cb26f0a8a8f4be77b34897bb37e63
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-07-23 22:46:22 +0530

    add Navbar along with styles

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Resume from './containers/resume';
 import Portfolio from './containers/portfolio';
 import Skills from './containers/skills';
 import Contact from './containers/contact';
+import Navbar from './components/navBar';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route index path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/resume' element={<Resume />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route index path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { FaReact, FaBars } from 'react-icons/fa';
+import { HiX } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import './styles.scss';
+
+const links = [
+  { label: 'Home', to: '/' },
+  { label: 'Skills', to: '/skills' },
+  { label: 'Portfolio', to: '/portfolio' },
+  { label: 'Contact', to: '/contact' },
+  { label: 'About me', to: '/about' },
+  { label: 'Resume', to: '/resume' },
+];
+
+const Navbar = () => {
+  const [toggleIcon, setToggleIcon] = useState(false);
+
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar__container'>
+        <Link to={links[0].to} className='navbar__container__logo'>
+          <FaReact size={30} />
+        </Link>
+      </div>
+      <ul className={`navbar__container__menu ${toggleIcon ? 'active' : ''}`}>
+        {links.map(link => (
+          <li className='navbar__container__menu__item' key={link.label}>
+            <Link to={link.to} className='navbar__container__menu__item__link'>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className='nav-icon' onClick={handleToggleIcon}>
+        {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
