7bf46bc44bedc3dab813b9acf4fbf248c0d6516b
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-07-30 22:00:17 +0530

    some style changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './containers/home';
-import About from './containers/about';
-import Resume from './containers/resume';
-import Portfolio from './containers/portfolio';
-import Skills from './containers/skills';
-import Contact from './containers/contact';
+// import Skills from './containers/skills';
+// import Contact from './containers/contact';
 import Navbar from './components/navBar';
 import './App.scss';
+
+// const Resume = lazy(() => import('./containers/resume'));
+const Home = lazy(() => import('./containers/home'));
+const Portfolio = lazy(() => import('./containers/portfolio'));
+const About = lazy(() => import('./containers/about'));
+const Skills = lazy(() => import('./containers/skills'));
 
 function App() {
   return (
@@ -14,14 +17,27 @@ function App() {
       <div className='app'>
         <Navbar />
         <div className='app__main-page-content'>
-          <Routes>
-            <Route index path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/resume' element={<Resume />} />
-            <Route path='/skills' element={<Skills />} />
-            <Route path='/portfolio' element={<Portfolio />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div
+                style={{ fontSize: 70, color: 'white' }}
+                className='app__loading_box'
+              >
+                <div class='ring'>
+                  <h1>Loading...</h1>
+                </div>
+              </div>
+            }
+          >
+            <Routes>
+              <Route index path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/skills' element={<Skills />} />
+              <Route path='/portfolio' element={<Portfolio />} />
+              {/* <Route path='/resume' element={<Resume />} /> */}
+              {/* <Route path='/contact' element={<Contact />} /> */}
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </Router>
